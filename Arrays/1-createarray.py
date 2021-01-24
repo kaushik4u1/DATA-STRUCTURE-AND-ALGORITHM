0327f16289c7266656383821518505be6486a994
Kaushik Mishra <mishrakaushik4@gmail.com>
2021-01-25 00:09:59 +0530

    Update 1-createarray.py

--- Arrays/1-createarray.py
+++ Arrays/1-createarray.py
@@ -2,7 +2,7 @@
 
 from array import*
 
-arr1 = array('i',[1,2,3,4,5,6])  #'i' used to define datatype(integer)
+arr1 = array('i',[1,2,3,4,5,6])  #'i' is used to define datatype(integer)
 print(arr1)
 
 Output: array('i',[1,2,3,4,5,6])
