4dc2933a829c857b786f7ffe3f7ea8fb60f93d42
Kaushik Mishra <mishrakaushik4@gmail.com>
2021-01-25 00:09:25 +0530

    Add files via upload

---
+++ Arrays/1-createarray.py
@@ -0,0 +1,8 @@
+# Create an array in Python.
+
+from array import*
+
+arr1 = array('i',[1,2,3,4,5,6])  #'i' used to define datatype(integer)
+print(arr1)
+
+Output: array('i',[1,2,3,4,5,6])
---
+++ Arrays/2-accessElement.py
@@ -0,0 +1,12 @@
+#Check if the asked array value is available in the given array.
+
+from array import*
+
+arr1 = array('i',[1,2,3,4,5,6,7])
+def accessElement(array,index):
+    if index >= len(array):
+         print("This index does not exist")
+    else:
+         print(array[index])
+
+accessElement(arr1,index)         
