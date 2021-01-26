fdbf2088ab10a05463c1a02c3ef3095bc56b48d3
Kaushik Mishra <mishrakaushik4@gmail.com>
2021-01-27 00:31:40 +0530

    Create 1-create2DArray.py

---
+++ Arrays/Two Dimensional array/1-create2DArray.py
@@ -0,0 +1,9 @@
+#Create a two dimensional array
+
+import numpy as np
+
+twoArray = np.array([[1,3,4,5],[11,32,43,54],[23,65,34,41],[22,32,43,54] ])
+print(twoArray)
+
+Time Complexity:O(mn)
+Space Complexity:O(mn)
