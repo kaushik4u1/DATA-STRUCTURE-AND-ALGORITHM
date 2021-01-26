bf532213735e927ba66fa664b7e9bdbfb2d810a5
Kaushik Mishra <mishrakaushik4@gmail.com>
2021-01-27 00:23:35 +0530

    Add files via upload

---
+++ Arrays/One Dimensional Array/5-traverseElement.py
@@ -0,0 +1,12 @@
+#Create an array and traverse it.
+
+from array import*
+
+arr1 = array('i',[1,2,3,4,5,6,7])
+def searchElement(array):
+    for i in array:
+         print(i)
+searchElement(arr1) 
+
+Time Complexity:O(n)
+Space Complexity:O(1)
