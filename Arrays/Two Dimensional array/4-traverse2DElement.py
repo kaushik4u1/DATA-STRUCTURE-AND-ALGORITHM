a63c6fb7bb2c803319fbb3fdd38c500e7346faf2
Kaushik Mishra <mishrakaushik4@gmail.com>
2021-01-27 11:43:35 +0530

    Create 4-traverse2DElement.py

---
+++ Arrays/Two Dimensional array/4-traverse2DElement.py
@@ -0,0 +1,12 @@
+#Travesing elements in 2D Array.
+
+import numpy as np
+twoArray = np.array([ [1,3,4,5],[11,32,43,54],[23,65,34,41],[22,32,43,54] ])
+def searchElement(array):
+    for i in range(len(array)): 
+         for j in range(len(array)):
+              print(array[i][j])
+searchElement(twoArray) 
+
+#Time Complexity:O(n^2)
+#Space Complexity:O(1)
