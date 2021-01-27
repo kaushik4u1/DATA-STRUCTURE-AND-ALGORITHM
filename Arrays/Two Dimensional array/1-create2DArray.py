87b2d5ac340bac5162960bf042b6b56a9fee7216
Kaushik Mishra <mishrakaushik4@gmail.com>
2021-01-27 11:12:34 +0530

    Update 1-create2DArray.py

--- Arrays/Two Dimensional array/1-create2DArray.py
+++ Arrays/Two Dimensional array/1-create2DArray.py
@@ -5,5 +5,5 @@ import numpy as np
 twoArray = np.array([[1,3,4,5],[11,32,43,54],[23,65,34,41],[22,32,43,54] ])
 print(twoArray)
 
-Time Complexity:O(mn)
-Space Complexity:O(mn)
+#Time Complexity:O(mn)
+#Space Complexity:O(mn)
