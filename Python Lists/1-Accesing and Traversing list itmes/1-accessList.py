3cb84ff17168041c2d5d20088bb3174d5d7330f1
Kaushik Mishra <mishrakaushik4@gmail.com>
2021-02-05 23:35:28 +0530

    Add files via upload

---
+++ Python Lists/1-Accesing and Traversing list itmes/1-accessList.py
@@ -0,0 +1,11 @@
+#Accessing items in a product list.
+
+products = ['milk','butter','rice','salt']
+
+print(products[3]+"\n"+products[2]) #by using "\n" we can print all the selective items in next line
+
+#Output:salt
+#       rice
+
+#Time Complexity:O(1)
+#Space Complexity:O(1) 
---
+++ Python Lists/1-Accesing and Traversing list itmes/2-traverseList.py
@@ -0,0 +1,15 @@
+#Traversing items in a product list.
+
+products = ['milk','butter','rice','salt']
+
+for items in products:
+   print(items)
+   
+#Output:milk
+#       butter
+#       rice
+#       salt   
+        
+        
+#Time Complexity:O(n)
+#Space Cokmplexity:O(1)
