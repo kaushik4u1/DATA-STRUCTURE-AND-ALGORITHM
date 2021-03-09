f298c27e4d7b63b43cdbe91d1a97d85f880403b0
Kaushik Mishra <mishrakaushik4@gmail.com>
2021-03-10 01:04:44 +0530

    Add files via upload

---
+++ Dictionaries(Pyhton)/1-create a dict.py
@@ -0,0 +1,15 @@
+#How to create a dictionary.
+
+engTosp = {"one":"uno","two":"dos","three":"tres"} #dict{}-can use to create a dictionary.
+print(engTosp)
+
+#Output: {'one': 'uno', 'two': 'dos', 'three': 'tres'}
+
+#If we want to access the value of first key:
+print(engTosp['one'])
+
+#Output: uno
+
+
+#Time Complexity:O(1)
+#Space Complexity:O(1)
