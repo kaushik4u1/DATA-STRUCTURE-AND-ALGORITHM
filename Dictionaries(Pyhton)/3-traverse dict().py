e43a75653e4ec552762816e87a8cb758d784f98a
Kaushik Mishra <mishrakaushik4@gmail.com>
2021-03-10 23:21:29 +0530

    Create 3-traverse dict().py

---
+++ Dictionaries(Pyhton)/3-traverse dict().py
@@ -0,0 +1,15 @@
+#How to traverse through a dictionary.
+
+myDict = {'Name':'Sam', 'Age':26, 'Address':'USA'} #dict{}-can use to create a dictionary.
+def traverseDict(dicti):
+    for key in dicti:
+        print(key, dicti[key])
+traverseDict(myDict)
+
+#Output: Name Sam
+#        Age 26
+#        Address USA
+
+
+#Time Complexity:O(1)
+#Space Complexity:O(1)
