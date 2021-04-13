f5c1de511a83dd59fd863899f04eff1287e82204
Kaushik Mishra <mishrakaushik4@gmail.com>
2021-04-13 15:18:21 +0530

    Create 2-Traversing Tuples.py

---
+++ Tuples(Python)/2-Traversing Tuples.py
@@ -0,0 +1,15 @@
+#How to traverse elements in tuples.
+
+ab = ('a','b','c','d','e','y')
+for i in ab:
+    print(i)
+    
+#Output:a
+#       b
+#       c
+#       d
+#       e
+#       y 
+
+#Time Complexity:O(n)
+#Space Complexity:O(1)
